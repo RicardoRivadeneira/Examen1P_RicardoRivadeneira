e5ed849cd3f4ccd910b856f11113586a950f59c6
RicardoRivadeneira <87491407+RicardoRivadeneira@users.noreply.github.com>
2023-12-13 08:50:08 -0500

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/EstudianteGraduado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    // Clase EstudianteGraduado que hereda de Estudiante
+    public class EstudianteGraduado : Estudiante
+    {
+        // Nueva propiedad de EstudianteGraduado
+        public string Titulo { get; set; }
+    }
+}
---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\t**Ejercicio 5: Herencia**");
+
+            EstudianteGraduado graduado1 = new EstudianteGraduado();
+
+            Console.Write("Ingrese el nombre del estudiante graduado:");
+            graduado1.Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese la edad del estudiante graduado:");
+            graduado1.Edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese la calificación del estudiante graduado:");
+            graduado1.Calificacion = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el título obtenido por el estudiante graduado:");
+            graduado1.Titulo = Console.ReadLine();
+
+            Console.WriteLine("\nInformación del Estudiante Graduado:");
+            graduado1.MostrarInformacion();
+            Console.WriteLine($"Título obtenido: {graduado1.Titulo}");
+
+            Console.ReadKey();
+        }
+    }
+}
