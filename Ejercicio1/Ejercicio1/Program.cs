1436a015f3957e1ce0b1973e6439c99f9702825d
RicardoRivadeneira <87491407+RicardoRivadeneira@users.noreply.github.com>
2023-12-13 08:48:57 -0500

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t**Ejercicio 1: Variables y Operador en C#**");
+            Console.WriteLine("Ejercicio con Datos Quemados");
+            // Parte con datos quemados
+            int numero1 = 24;
+            int numero2 = 14;
+
+            int resultado = numero1 + numero2;
+            Console.WriteLine(numero1 + " + " + numero2);
+            Console.WriteLine("El resultado de la suma con datos quemados es: " + resultado);
+
+            Console.WriteLine("\nEjercicio con Ingreso de Datos");
+            // Solicitar al usuario ingresar valores
+            Console.Write("Por favor, ingrese el primer número: ");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Por favor, ingrese el segundo número: ");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            int userResult = userInput1 + userInput2;
+
+            Console.WriteLine("El resultado de la suma con los números ingresados es: " + userResult);
+
+            Console.ReadKey(); // Detener la consola para visualizar el resultado
+        }
+    }
+}
---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Estudiante
+    {
+        // Propiedades de la clase Estudiante
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public double Calificacion { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\t**Ejercicio 3: Clases y Objetos**");
+            Console.WriteLine("Ejercicio con Datos Quemados");
+            // Crea un objeto de tipo Estudiante llamado estudiante1 con datos quemados
+            Estudiante estudiante1 = new Estudiante
+            {
+                Nombre = "Ricardo Rivadeneira",
+                Edad = 24,
+                Calificacion = 18.5
+            };
+
+            // Imprime en la consola la información del estudiante con datos quemados
+            Console.WriteLine("Información del Estudiante con datos quemados:");
+            Console.WriteLine($"Nombre: {estudiante1.Nombre}");
+            Console.WriteLine($"Edad: {estudiante1.Edad} años");
+            Console.WriteLine($"Calificación: {estudiante1.Calificacion}");
+
+            Console.WriteLine("\nEjercicio con Ingreso de Datos");
+            // Solicita al usuario ingresar información del estudiante
+            Console.Write("Ingrese el nombre del estudiante:");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese la edad del estudiante:");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese la calificación del estudiante:");
+            double calificacion = Convert.ToDouble(Console.ReadLine());
+
+            // Crea un nuevo objeto Estudiante con la información ingresada por el usuario
+            Estudiante estudiante2 = new Estudiante
+            {
+                Nombre = nombre,
+                Edad = edad,
+                Calificacion = calificacion
+            };
+
+            // Imprime en la consola la información del estudiante ingresada por el usuario
+            Console.WriteLine("\nInformación del Estudiante ingresada por el usuario:");
+            Console.WriteLine($"Nombre: {estudiante2.Nombre}");
+            Console.WriteLine($"Edad: {estudiante2.Edad} años");
+            Console.WriteLine($"Calificación: {estudiante2.Calificacion}");
+
+            Console.ReadKey();
+        }
+    }
+}
